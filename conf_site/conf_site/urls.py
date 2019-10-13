8cd10421414da08e4c7efb0ba63f0f8b14c413f8
Jaskaran Singh <sjaskaran10@yandex.com>
2019-10-13 20:04:14 +0530

    Added basic styling and Put paid and free conferences into separate pages

--- conf_site/conf_site/urls.py
+++ conf_site/conf_site/urls.py
@@ -19,5 +19,6 @@ from god import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('paid/', views.paid, name='paid')
 ]
--- conf_site/god/views.py
+++ conf_site/god/views.py
@@ -10,24 +10,33 @@ def home(request):
 
     conf_data = response.json()
 
-    paid_data = conf_data['paid']
     free_data = conf_data['free']
-    paid_conf = []
     free_conf = []
 
     for conf_no in range(len(free_data)):
         newDicts = {key: value for (key, value) in free_data[conf_no].items(
-        ) if key == 'imageURL' or key == 'confEndDate' or key == 'confStartDate' or key == 'confName' or key == 'venue'}
-        newDicts['status'] = 'free'
+        ) if key == 'imageURL' or key == 'confEndDate' or key == 'confStartDate' or key == 'confName' or key == 'venue' or key == 'confUrl'}
         free_conf.append(newDicts)
+
+    return render(request, 'god/index.html', {
+        'free': free_conf
+    })
+
+
+def paid(request):
+    response = requests.get(
+        'https://o136z8hk40.execute-api.us-east-1.amazonaws.com/dev/get-list-of-conferences')
+
+    conf_data = response.json()
+
+    paid_data = conf_data['paid']
+    paid_conf = []
 
     for conf_no in range(len(paid_data)):
         newDicts = {key: value for (key, value) in paid_data[conf_no].items(
-        ) if key == 'imageURL' or key == 'confEndDate' or key == 'confStartDate' or key == 'confName' or key == 'venue'}
-        newDicts['status'] = 'paid'
+        ) if key == 'imageURL' or key == 'confEndDate' or key == 'confStartDate' or key == 'confName' or key == 'venue' or key == 'confUrl'}
         paid_conf.append(newDicts)
 
-    return render(request, 'god/index.html', {
-        'paid': paid_conf,
-        'free': free_conf
+    return render(request, 'god/paid.html', {
+        'paid': paid_conf
     })
